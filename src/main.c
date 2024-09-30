623697e47c49458b3c019471e93c56372a3aa4a6
oto <oto@15999>
2024-09-29 20:31:02 -0600

    no comment

--- src/main.c
+++ src/main.c
@@ -2,37 +2,34 @@
 #include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
-
+#define MAX_INPUT_SIZE 256
 
 int main(){
     pruebas();
-	puts("Welcome!");
+	puts("Commands => h for history | #num for specific history | q for quit");
 		
 	List *list = init_history();
-	char c[100];
+	char input[MAX_INPUT_SIZE];
 	char count = 0;
 	while(1){
-		fputs("\nProvide your input to tokenize (h for history | #num for specific history | q for quit): ", stdout);
+		fputs("\n>", stdout);
 		fflush(stdout);
-		//char c[100];
-		//memset(c, '\0', sizeof(c));
-		//char count = 0;
 		char count_in = count;
 
 		char curr_char = getchar();
 
 		while(curr_char == '\n');
 		while(curr_char != '\n'){
-			c[count] = curr_char;
+			input[count] = curr_char;
 			curr_char = getchar();
 			count++;
 		}
 		
-		if (c[count_in] == 'h' && (count-count_in) == 1){
+		if (input[count_in] == 'h' && (count-count_in) == 1){
 			printf("Printing full History: \n");
 			print_history(list);			
-		}else if (c[count_in] == '#' && (count-count_in) == 2){
-			int num = (int)c[count_in+1] -48;
+		}else if (input[count_in] == '#' && (count-count_in) == 2){
+			int num = (int)input[count_in+1] -48;
 			printf("Getting item %d from History: \n%d:", num, num);
 			char *word = get_history(list, num);
 			while (*word != 0){
@@ -40,13 +37,13 @@ int main(){
 				word++;
 			}
 			putchar('\n');
-		}else if (c[count_in] == 'q' && (count-count_in) == 1){
+		}else if (input[count_in] == 'q' && (count-count_in) == 1){
 			break;
 		}else{
 			printf("Tokenizing your input: \n");
-			char **tokens = tokenize(&c[count_in]);
+			char **tokens = tokenize(&input[count_in]);
 			print_tokens(tokens);
-			add_history(list, &c[count_in]);
+			add_history(list, &input[count_in]);
 		}
 		count ++;	
 	}
